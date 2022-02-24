0ccf7d228105d10d3ea07bb80b6dbeeb2a3e0400
ethankoestler <91089973+ethankoestler@users.noreply.github.com>
2022-02-24 14:30:08 -0600

    Added url connections between pages

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from flask import *
 
 app = Flask(__name__, static_folder='static')
 
-@app.route('/')
+@app.route('/main')
 def main():
     return render_template('main.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/program_search')
+def program_search():
+    return render_template('program_search.html')
 
 with app.test_request_context():
     print(url_for('main'))
